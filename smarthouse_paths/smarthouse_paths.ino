54113cee917b12404e6e3f2df491c69a8c64a320
christian riekoff <info@texone.org>
2018-02-06 12:17:13 +0100

    finished path demo

--- smarthouse_paths/smarthouse_paths.ino
+++ smarthouse_paths/smarthouse_paths.ino
@@ -1,37 +1,31 @@
 //--- code ---
 /*
-  Motor drive pulse/dir output to two axes with linear accel/decel
+  Initial position is at the outer bottom
+
+  
 */
 //Declares
-const int ledPin =  LED_BUILTIN; // #13=the number of the LED pin
+const int LED_PIN =  LED_BUILTIN; // #13=the number of the LED pin
 
-const int StepPinA = 9;   //motor1
-const int DirPinA = 10;   // motor1
-const int StepPinZ = 11;  //motor2
-const int DirPinZ = 12;   // motor2
+const int STEP_PIN_A = 9;   //motor1
+const int DIR_PIN_A = 10;   // motor1
+const int STEP_PIN_Z = 11;  //motor2
+const int DIR_PIN_Z = 12;   // motor2
 
-const int homeA = 6;
-const int homeZ = 8;
+const int HOME_A = 6;
+const int HOME_Z = 8;
 
 //motor 1 config (A)
-float intervalA = 1000;       // interval at which to step (micro seconds)
-const float nA = 100;         // Top speed motor 1 -MicroS between pulses
-const float aA = 0.1;         // accel rate, motor 1
-long stepAcounter = 0;
-long maxstepsA = 126800;      //motor 1
+const long MAX_STEPS_A = 126800;      //motor 1
+long lastStepA;
 boolean dirAstate = true;
 boolean stepAstate = false;
-boolean stoppingA;
 
 //motor 2 config (Z)
-float intervalZ = 1000;       // interval at which to step (micro seconds)
-const float nZ = 250;         // Top speed; motor 2 time between pulses
-const float aZ = 0.1;         // accel rate, motor 2
-long stepZcounter = 0;
-long maxstepsZ = 30000;       //motor 2
+const long MAX_STEPS_Z = 30000;       //motor 2
+long lastStepZ;
 boolean dirZstate = true;
 boolean stepZstate = false;
-boolean stoppingZ;
 
 int Homed = 0;                  // initialize as un-homed
 unsigned long previousTmA = 0;  // will store last time motor1 was updated
@@ -42,41 +36,49 @@ float watchdog;
 
 
 unsigned long _myStart;
-long _myLastStep;
+
+double _myProgress;
+
+double _myXStart = 0;
+double _myYStart = 0;
+  
+double _myXEnd = 0;
+double _myYEnd = 0;
+  
+long myLastMicros;
+
+// change this value to slow things down or speed them up
+const double PERIOD_MICROS = 6. * 1000. * 1000.;
+
+const double MAX_ANGLE = radians(175.0 / 2.);
+const double MAX_ANGLE_STEPS = MAX_STEPS_A / 2;
 
 void setup() {
   Serial.begin(9600);
   // set the digital pin as output:
-  pinMode(ledPin, OUTPUT);
-  pinMode(StepPinA, OUTPUT);
-  pinMode(DirPinA, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(STEP_PIN_A, OUTPUT);
+  pinMode(DIR_PIN_A, OUTPUT);
 
-  pinMode(StepPinZ, OUTPUT);
-  pinMode(DirPinZ, OUTPUT);
+  pinMode(STEP_PIN_Z, OUTPUT);
+  pinMode(DIR_PIN_Z, OUTPUT);
 
-  pinMode(homeA, INPUT_PULLUP); // connect: Arduino pin6 & GND
-  pinMode(homeZ, INPUT_PULLUP); // connect: Arduino pin8 & GND
-  digitalWrite(ledPin, HIGH);
+  pinMode(HOME_A, INPUT_PULLUP); // connect: Arduino pin6 & GND
+  pinMode(HOME_Z, INPUT_PULLUP); // connect: Arduino pin8 & GND
+  digitalWrite(LED_PIN, HIGH);
 
-  digitalWrite(DirPinZ, LOW); //set direction up
+  digitalWrite(DIR_PIN_Z, LOW); //set direction up
 
   //Z axis up
 
   // leave element at the outer limit for this test
-  /*
-    for (long nn = 0; nn < (maxstepsZ + 2000); nn ++) { // raise z off the bottom before running...
-    digitalWrite(StepPinZ, !digitalRead(StepPinZ)); //step motor 2 upwards before run
-    delayMicroseconds(300);//speed limiter
-    }*/
-
-  //A axis half turn
-  for (long nn = 0; nn < maxstepsA / 2; nn ++) { // raise A to go half off center before running...
-    digitalWrite(StepPinA, ! digitalRead(StepPinA));
-    delayMicroseconds(100);//speed limiter
-  }
-  _myLastStep = maxstepsA / 2;
-  delay(3000);//pause for 3 seconds
+  // set end positions according to outer limits call setTargetPosition will 
+  // correctly initialize start and end
+  _myXEnd = x(0,1);
+  _myYEnd = y(0,1);
+ 
   _myStart = micros();
+  setTargetPosition();
 
 }// void setup end
 
@@ -105,30 +107,78 @@ double blend(double theStart, double theEnd, double theBlend){
   return theStart * (1 - theBlend) + theEnd * theBlend;
 }
 
-// change this value to slow things down or speed them up
-const double _myPeriodMicros = 600. * 1000. * 1000.;
-
+void setTargetPosition(){
+  _myXStart = _myXEnd;
+  _myYStart = _myYEnd;
+      
+  double myAngle = random(-100000,100000) / 100000. * MAX_ANGLE;
+  double myRadius = random(66666,100000) / 100000.;
+      
+  _myXEnd = x(myAngle, myRadius);
+  _myYEnd = y(myAngle, myRadius);
+      
+  _myProgress = 0;
+  myLastMicros = micros() - _myStart;
+}
 
 void loop() {
   /* while (Homed < 2){ // check home status for homed ok and estop (maybe)
      //wait in this loop
-     Homed=digitalRead(homeA);
-     Homed = Homed + digitalRead(homeZ); // two motor switches at 1 = 2, so ok to go...
+     Homed=digitalRead(HOME_A);
+     Homed = Homed + digitalRead(HOME_Z); // two motor switches at 1 = 2, so ok to go...
     }
   */
-
+  if(_myProgress > 1.){
+    setTargetPosition();
+  }
 
   unsigned long currentMicros = micros(); // take time snapshot
-  double myTime = double((currentMicros - _myStart)) / _myPeriodMicros;
-  double myCos = cos(myTime * TWO_PI);
-  long myStep = long(myCos * (maxstepsA / 2));
-  dirAstate = myStep > _myLastStep;
-  int mySteps = abs(myStep - _myLastStep);
-  Serial.println(myStep);
-  _myLastStep = myStep;
-  digitalWrite(DirPinA, dirAstate);
-  for (int i = 0; i < mySteps; i++) {
+  double myEasedBlend = easeInOut(_myProgress);
+    
+  double myBlendX = blend(_myXStart, _myXEnd, myEasedBlend);
+  double myBlendY = blend(_myYStart, _myYEnd, myEasedBlend);
+    
+  double myBlendAngle = angle(myBlendX, myBlendY);
+  double myBlendRadius = radius(myBlendX, myBlendY);
+
+  /*
+  Serial.print(degrees(myBlendAngle));
+  Serial.print(",");
+  Serial.println(myBlendRadius);
+  */
+    
+  long myStepA = long(myBlendAngle / MAX_ANGLE * MAX_ANGLE_STEPS);
+  long myStepZ = long((1 - myBlendRadius) * MAX_ANGLE_STEPS);
+  //  _myPosition.set(x(myBlendAngle, myRadius * 300), y(myBlendA
+  //ngle, myRadius * 300));
+/*
+   Serial.print(myStepA);
+   Serial.print(",");
+   Serial.println(myStepZ);
+   */
+  _myProgress = double((currentMicros - myLastMicros)) / PERIOD_MICROS;
+  
+  dirAstate = myStepA > lastStepA;
+  int myStepsA = abs(myStepA - lastStepA);
+  lastStepA = myStepA;
+  digitalWrite(DIR_PIN_A, dirAstate);
+  for (int i = 0; i < myStepsA; i++) {
     stepAstate = !stepAstate;
-    digitalWrite(StepPinA, stepAstate);
+    digitalWrite(STEP_PIN_A, stepAstate);
   }
+
+  dirZstate = myStepZ > lastStepZ;
+  int myStepsZ = abs(myStepZ - lastStepZ);
+  lastStepZ = myStepZ;
+  digitalWrite(DIR_PIN_Z, dirZstate);
+  for (int i = 0; i < myStepsZ; i++) {
+    stepZstate = !stepZstate;
+    digitalWrite(STEP_PIN_Z, stepZstate);
+  }
+  /*
+  Serial.print(",");
+  Serial.print(_myYStart);
+  Serial.print(",");
+  */
+ // Serial.println(myStepA + ","+ myStepZ);
 }
