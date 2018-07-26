42f3d2a62db7d6a382a115b3ab8033e2758a486d
christian riekoff <info@texone.org>
2018-07-26 15:02:29 +0200

    move math functions to separate header file

---
+++ smarthouse_rotation_only/Math.h
@@ -0,0 +1,17 @@
+#ifndef __Math__
+#define __Math__
+
+double saturate(double val){
+  return max(0,min(val, 1));
+}
+
+double smoothStep(double edge0, double edge1, double x) {
+    double t = saturate((x - edge0) / (edge1 - edge0));
+    return t * t * (3.0 - 2.0 * t);
+}
+
+double dRandom() {
+  return random(0, 100000) / 100000. ;
+}
+
+#endif
